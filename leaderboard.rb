245d3cbe253147e30a3dae344771c24490421446
Stefan Boesen <stefan.boesen@gmail.com>
2014-06-24 13:09:58 -0700

    Add leaderboard

---
+++ leaderboard.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+
+# run from points dir
+get '/' do
+  teams = {}
+  Dir.foreach('.') do |filename|
+    next if filename == "." || filename == '..'
+    score = File.read(filename).to_i
+    team = filename.match(/Team_\d+/).to_s
+    next if team.nil? || team == ""
+    teams[team] = score
+  end
+  sorted = teams.sort_by { |k,v| -v }
+  out = ""
+  sorted.each do |team, points|
+    out += "#{team}: #{points}\n"
+  end
+  out
+end
